e7146c66b5af312276eee9b271ca79f4810639f2
Merrii <merrii@bk.ru>
2020-04-15 08:15:44 +0200

    Very first commit

---
+++ _footer.php
@@ -0,0 +1,17 @@
+<?php
+if( !defined( 'CUSTOMER_PAGE' ) )
+  exit;
+?>
+        <div id="options"><div class="print"><a href="javascript:window.print();"><?php echo $lang['print']; ?></a></div><div class="back"><a href="javascript:history.back();">&laquo; <?php echo $lang['back']; ?></a></div></div>
+      </div>
+    </div>
+  </div>
+  <div id="foot"><?php // footer starts here ?>
+    <div class="container">
+      <div id="copy"><?php echo $config['foot_info']; ?></div><!-- copyrights here -->
+      <div class="foot" id="powered"><a href="./"><img src="<?php echo $config['dir_skin']; ?>img/quick.cart.png" alt="Script logo" width="187" height="15" /></a></div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
